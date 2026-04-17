8e811593cf9046bc415185c187fda9569bdc754e
martinsbruno2026 <martinsbru08@gmail.com>
2026-04-17 11:05:04 -0300

    update GRANDAO

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -5,3 +5,31 @@ class LivroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'  # ou liste os campos: ['id', 'titulo', 'isbn', 'quantidade', 'preco']
+
+        ...
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
+...
+class LivroRetrieveSerializer(ModelSerializer):
+    capa = ImageSerializer(required=False)
+
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1
+...
+class LivroSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source='capa',
+        queryset=Image.objects.all(),
+        slug_field='attachment_key',
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(required=False, read_only=True)
+
+    class Meta:
+        model = Livro
+        fields = '__all__'
