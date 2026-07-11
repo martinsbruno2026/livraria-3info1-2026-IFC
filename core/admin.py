6f5b34a4b2c518641774e251e199cacd5414e6c1
martinsbruno2008 <martinsbru08@gmail.com>
2026-07-11 09:17:25 -0300

    feat: criação dos itens da compra

--- core/admin.py
+++ core/admin.py
@@ -55,8 +55,7 @@ admin.site.register(models.Autor)
 admin.site.register(models.Categoria)
 admin.site.register(models.Editora)
 admin.site.register(models.Livro)
-<<<<<<< HEAD
 admin.site.register(models.User, UserAdmin)
-=======
 admin.site.register(models.User, UserAdmin)
->>>>>>> 289b2b8760bcde2e74635f8492ad2e3ddcee4bfd
+admin.site.register(models.Compra)
+
